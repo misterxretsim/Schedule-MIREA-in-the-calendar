5c88b78c9b6420461dc189381daea0d1a9eda64e
georgijfalileev <gosha-falileev@mail.ru>
2018-05-14 12:39:40 +0300

    Какая-то обнова с Seeker and SeekerType + InputSeeker

--- src/main/java/ru/gosha/Main.java
+++ src/main/java/ru/gosha/Main.java
@@ -2,12 +2,19 @@ package ru.gosha;
 
 import ru.gosha.SG_Muwa.*;
 import java.io.IOException;
+import java.text.ParseException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         OpenFile openFile = new OpenFile();
         System.out.println(openFile.Open("/Users/georgijfalileev/Downloads/Example2.xlsx"));
         System.out.println(openFile.Open("/Users/georgijfalileev/Downloads/Example1.xls"));
+        InputSeeker inputSeeker = new InputSeeker();
+        Seeker seeker = inputSeeker.setSeeker();
+        System.out.print(seeker.DefaultAddress+" "+seeker.NameOfSeeker+" "+seeker.dateStart+" ");
+        System.out.print(seeker.dateFinish+" "+seeker.seekerType.getStudyGroup()+" ");
+        System.out.print(seeker.seekerType.getTeacher());
+
 
     }
 }
