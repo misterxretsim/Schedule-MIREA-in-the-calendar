56ad95a949675df69ad22dd8c1c8d7f850d823ce
georgijfalileev <gosha-falileev@mail.ru>
2018-05-19 20:35:02 +0300

    Какая-то обнова с Seeker and SeekerType + InputSeeker

--- src/main/java/ru/gosha/Main.java
+++ src/main/java/ru/gosha/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         OpenFile openFile = new OpenFile();
         System.out.println(openFile.Open("delete1.xls", 1, 2));
-        System.out.println(openFile.Open("delete1.xls", 1, 1));
+        System.out.println(openFile.Open("delete1.xls", 2, 1));
         InputSeeker inputSeeker = new InputSeeker();
         Seeker seeker = inputSeeker.setSeeker();
         System.out.print(seeker.DefaultAddress+" "+seeker.NameOfSeeker+" "+seeker.dateStart+" ");
