927589d4b07e665951eeeed1611ce2c004c21663
georgijfalileev <gosha-falileev@mail.ru>
2018-06-07 09:36:42 +0300

    Добавлена функция отправки сообщения клиенту

--- server-exel/src/main/java/ru/gosha/CouplesDetective/DetectiveException.java
+++
@@ -1,7 +0,0 @@
-package ru.gosha.CouplesDetective;
-
-public class DetectiveException extends Exception {
-    DetectiveException(String message){
-        super(message);
-    }
-}
--- server-exel/src/main/java/ru/gosha/CouplesDetective/ExportCouplesToICal.java
+++
@@ -1,9 +0,0 @@
-package ru.gosha.CouplesDetective;
-
-import java.util.List;
-
-public class ExportCouplesToICal {
-    public static String start(Iterable<Couple> couples) {
-        return ""; // TODO.
-    }
-}
