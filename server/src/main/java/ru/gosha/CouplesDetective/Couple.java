e2c2e796b9c0f3e4ae449276d6ba332f9a21bf39
SG_Muwa <motherlode.muwa@gmail.com>
2018-06-12 20:45:26 +0300

    Тестирования Couple
    #9 Добавлено два с половиной тестирование Couple.

--- server/src/main/java/ru/gosha/CouplesDetective/Couple.java
+++ server/src/main/java/ru/gosha/CouplesDetective/Couple.java
@@ -1,15 +1,18 @@
 package ru.gosha.CouplesDetective;
 
-import java.time.LocalDate;
-import java.time.DayOfWeek;
-import java.time.LocalDateTime;
+import java.time.*;
+import java.util.List;
 
 
 public class Couple {
     /**
      * Дата и время пары.
      */
-    public LocalDateTime DateAndTimeOfCouple;
+    public OffsetDateTime DateAndTimeOfCouple;
+    /**
+     * Количество времени, сколько длится пара.
+     */
+    public Duration DurationOfCouple;
     /**
      * Название группы.
      */
@@ -41,22 +44,24 @@ public class Couple {
      * @param finish Дата и время окончания сессии. Расписание будет составлено до этого дня и времени.
      * @param timeStartOfCouple Время начала пары.
      * @param timeFinishOfCouple Время окончания пары.
+     * @param timezone Часовой пояс, в котором указано время в расписании. Указывается в секундах.
      * @param nameOfGroup Рассматриваемая группа.
-     * @param dayOfWeek Рассматриваемый день недели. Использование: Напрмер, Calendar.MUNDAY
-     * @param ItemTitle Первая строка данных названия предмета. Сюда может входить и номера недель.
+     * @param dayOfWeek Рассматриваемый день недели. Использование: Напрмер, Calendar.MUNDAY.
+     * @param isOdd True, если это для не чётной недели. False, если эта строка для чётной недели.
+     * @param itemTitle Первая строка данных названия предмета. Сюда может входить и номера недель.
      * @param typeOfLesson Первая строка типа занятия.
      * @param nameOfTeacher Первая строка данных преподавателя.
      * @param audience Первая строка аудитории.
      * @param address Адрес корпуса.
      * @return Возвращает, в какие дни будут пары.
      */
-    public static Iterable<Couple> GetCouplesByPeriod(LocalDate start, LocalDate finish, int timeStartOfCouple, int timeFinishOfCouple, DayOfWeek dayOfWeek, String nameOfGroup, String ItemTitle, String typeOfLesson, String nameOfTeacher, String audience, String address) {
+    public static List<Couple> GetCouplesByPeriod(LocalDate start, LocalDate finish, LocalTime timeStartOfCouple, LocalTime timeFinishOfCouple, int timezone, DayOfWeek dayOfWeek, boolean isOdd, String nameOfGroup, String itemTitle, String typeOfLesson, String nameOfTeacher, String audience, String address) {
         ///.get(Calendar.DAY_OF_WEEK);
         // TODO: Данная функция ещё не разработана.
-        ItemTitle = ItemTitle.trim();
-        if(ItemTitle.contains("н."))
+        itemTitle = itemTitle.trim();
+        if(itemTitle.contains("н."))
         {
-            if(ItemTitle.contains("кр."));
+            if(itemTitle.contains("кр."));
         }
         return null;
     }
--- server/src/test/java/CoupleTest.java
+++ server/src/test/java/CoupleTest.java
@@ -2,26 +2,99 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import ru.gosha.CouplesDetective.Couple;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.time.DayOfWeek;
+import java.time.*;
+import java.util.List;
 
 public class CoupleTest {
 
+    /**
+     * Тестирование, если у нас одна пара в один день.
+     */
     @Test
-    public void startTestCouple() {
+    public void startTestOneCouple() {
 
-        LocalDate start = LocalDate.of(2000, Month.JANUARY, 1);
-        LocalDate finish = LocalDate.of(2000, Month.JANUARY, 1 + 14 + 14);
+        LocalDate start = LocalDate.of(2017, Month.DECEMBER, 31);
+        LocalDate finish = LocalDate.of(2018, Month.JANUARY, 2);
 
-        int time1 = 1;
-        int time2 = 3; // Гыыы, пара длится 2 минуты
+        LocalTime time1 = LocalTime.of(0, 1, 0);
+        LocalTime time2 = LocalTime.of(0, 3, 0); // Гыыы, пара длится 2 минуты
 
         DayOfWeek day = DayOfWeek.MONDAY;
 
+        int timezone = 0; // GMT+0:00
+
         String nGr = "Группа-01 32";
         String nam = "Математика и инженерия."; // http://xpoint.ru/forums/internet/standards/thread/29138.xhtml
+        String typ = "Лк";
+        String tic = "Иванов В.В.";
+        String add = "Москва, проспект Вернадского 78, РТУ МИРЭА";
+        String aud = "А-1";
 
-        Couple.GetCouplesByPeriod(start, finish, time1, time2, day, nGr, );
+        List<Couple> out = Couple.GetCouplesByPeriod(start, finish, time1, time2, timezone, day, true, nGr, nam, typ, tic, aud, add);
+
+        /* Количество */            assertEquals(1, out.size());
+        /* Время начала пары*/      assertEquals(OffsetDateTime.of(LocalDateTime.of(LocalDate.of(2018, Month.JANUARY, 1), LocalTime.of(0, 1, 0)), ZoneOffset.ofTotalSeconds(timezone)), out.get(0).DateAndTimeOfCouple);
+        /* Продолжительность пары*/ assertEquals(Duration.between(time1, time2), out.get(0).DurationOfCouple);
+        /* Название группы */       assertEquals(nGr, out.get(0).NameOfGroup);
+        /* Название предмета */     assertEquals(nam, out.get(0).ItemTitle);
+        /* Тип пары */              assertEquals(typ, out.get(0).TypeOfLesson);
+        /* Имя преподавателя*/      assertEquals(tic, out.get(0).NameOfTeacher);
+        /* Адрес кампуса */         assertEquals(add, out.get(0).Address);
+        /* Аудитория */             assertEquals(aud, out.get(0).Audience);
+    }
+
+    /**
+     * Тестирование, если у нас одна пара на один конкретный день дней.
+     */
+    @Test
+    public void startTestOneHardCouple() {
+
+        LocalDate start = LocalDate.of(2018, Month.JANUARY, 1);
+        LocalDate finish = LocalDate.of(2018, Month.JANUARY, 1);
+
+        LocalTime time1 = LocalTime.of(0, 1, 0);
+        LocalTime time2 = LocalTime.of(0, 3, 0); // Гыыы, пара длится 2 минуты
+
+        DayOfWeek day = DayOfWeek.MONDAY;
+
+        int timezone = 0; // GMT+0:00
+
+        String nGr = "Группа-24 32";
+        String nam = "Русский jaja номер Нан 1 4 2."; // http://xpoint.ru/forums/internet/standards/thread/29138.xhtml
+        String typ = "Лаб.";
+        String tic = "Момов А.А.";
+        String add = "Україна, Київ, Центральна 8";
+        String aud = "202";
+
+        List<Couple> out = Couple.GetCouplesByPeriod(start, finish, time1, time2, timezone, day, true, nGr, nam, typ, tic, aud, add);
+
+        /* Количество */            assertEquals(1, out.size());
+        /* Время начала пары*/      assertEquals(OffsetDateTime.of(LocalDateTime.of(LocalDate.of(2018, Month.JANUARY, 1), LocalTime.of(0, 1, 0)), ZoneOffset.ofTotalSeconds(timezone)), out.get(0).DateAndTimeOfCouple);
+        /* Продолжительность пары*/ assertEquals(Duration.between(time1, time2), out.get(0).DurationOfCouple);
+        /* Название группы */       assertEquals(nGr, out.get(0).NameOfGroup);
+        /* Название предмета */     assertEquals(nam, out.get(0).ItemTitle);
+        /* Тип пары */              assertEquals(typ, out.get(0).TypeOfLesson);
+        /* Имя преподавателя*/      assertEquals(tic, out.get(0).NameOfTeacher);
+        /* Адрес кампуса */         assertEquals(add, out.get(0).Address);
+        /* Аудитория */             assertEquals(aud, out.get(0).Audience);
+    }
+
+    /**
+     * Тестирование, если у нас одна пара в несколько дней.
+     */
+    @Test
+    public void startTestOneCoupleTwoDays() {
+
+        LocalDate start = LocalDate.of(2018, Month.JANUARY, 1);
+        LocalDate finish = LocalDate.of(2018, Month.JANUARY, 1 + 14);
+
+        LocalTime time1 = LocalTime.of(9, 0, 0);
+        LocalTime time2 = LocalTime.of(10, 30, 0); // Гыыы, пара длится 2 минуты
+
+        DayOfWeek day = DayOfWeek.MONDAY;
+
+        int timezone = 0; // GMT+0:00
+
+        String nGr = "Группа-,";
     }
 }
